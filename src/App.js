2397b86064d7208bb31edb983b50825d56a451cc
ooreally <anu1512sri@gmail.com>
2021-05-13 17:52:00 +0530

    ADD:  Viewers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 //import { Counter } from './features/counter/Counter';
 import Header from './components/Header';
-//import Home from './components/Home';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
        <Header/>
-       {/* <Home/> */}
+       <Home/>
     </div>
   );
 }
